c767661d97341f40d74f605ac3776af7ee35296d
John Stracke <francis@thibault.org>
2021-05-15 20:30:28 -0400

    Loading photos.json on the fly

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 function loadPhotos() {
+    var request = new XMLHttpRequest();
+    request.open("GET", "photos/photos.json", true);
+    request.onreadystatechange = function() {
+        if ( request.readyState === 4 && request.status === 200 ) {
+            var photos = JSON.parse(request.responseText);
+            console.log(photos);
+            showPhotos(photos);
+        }
+    };
+    request.send(null);
+}
+
+function showPhotos(photos) {
     var ctx=document.getElementById('canvas').getContext('2d')
     var img=new Image();
     img.addEventListener('load', function() {
         ctx.drawImage(img, 0, 0);
     });
-    img.src="thumbnails/2019:08:03T10:18:33-cv-castle.jpg";
+    img.src='thumbnails/'+photos[0]['File'];
 }

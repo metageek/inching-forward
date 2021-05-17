8ddf084a890750514d8585717cbd2b8528d4d428
John Stracke <francis@thibault.org>
2021-05-16 20:10:03 -0400

    Placing the photos
    Laying them out, maintaining a data structure with the descriptions, so
    I can get more dynamic later.

--- index.js
+++ index.js
@@ -2,24 +2,41 @@ function loadPhotos() {
     var request = new XMLHttpRequest();
     request.open("GET", "photos/photos.json", true);
     request.onreadystatechange = function() {
-        if ( request.readyState === 4 && request.status === 200 ) {
+        if (request.readyState === 4 && request.status === 200) {
             var photos = JSON.parse(request.responseText);
-            console.log(photos);
-            showPhotos(photos);
+            parsePhotos(photos);
         }
     };
     request.send(null);
 }
 
-function showPhotos(photos) {
+function parsePhotos(photos) {
     var ctx=document.getElementById('canvas').getContext('2d')
+	photos.sort(function(a, b) {
+		if (a["File"] < b["File"]) { return -1; }
+		if (a["File"] > b["File"]) { return 1; }
+		return 0;
+	});
+	var imgs = {};
+	photos.reverse();
+	var numLeft=photos.length;
     photos.forEach(function(photo) {
         var img=new Image();
         img.addEventListener('load', function() {
-            var x = photo['X'];
+			numLeft -= 1;
+			var x = photo['X'];
             var y = photo['Y'];
-            if (x != null && y != null) {
-                ctx.drawImage(img, x, y);
+			imgs[photo["File"]] = [img, x, y];
+			if (numLeft == 0) {
+				photos.forEach(function (p) {
+					var imgSpec=imgs[p["File"]];
+					var img = imgSpec[0];
+					var x = imgSpec[1];
+					var y = imgSpec[2];
+					if (x != null && y != null) {
+						ctx.drawImage(img, x, y);
+					}
+				});
             }
         });
         img.src='thumbnails/'+photo['File'];
